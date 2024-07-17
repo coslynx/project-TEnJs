be630fffa347d177a292f698691b34c88bef856c
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-17 08:27:55 +0530

    Generated: `discord-bot-project/src/commands/nowPlayingCommand.js`

---
+++ discord-bot-project/src/commands/nowPlayingCommand.js
@@ -0,0 +1,7 @@
+const nowPlayingCommand = () => {
+    // Logic to retrieve currently playing song information
+    // This function should interact with the musicPlayback module
+    // Retrieve the current song information and format it for display in Discord chat
+};
+
+module.exports = nowPlayingCommand;
