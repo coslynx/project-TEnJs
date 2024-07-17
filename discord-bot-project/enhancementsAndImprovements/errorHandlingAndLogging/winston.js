838ac200f61287ea965dcafab9518851bf3bd68b
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-17 08:35:12 +0530

    Generated: `discord-bot-project/enhancementsAndImprovements/errorHandlingAndLogging/winston.js`

---
+++ discord-bot-project/enhancementsAndImprovements/errorHandlingAndLogging/winston.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+
+// Create a logger instance
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+module.exports = logger;
