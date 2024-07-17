2dedc626bf1a37a735221b45891729701b8a7990
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-17 08:28:42 +0530

    Generated: `discord-bot-project/features/musicPlayback/spotify.js`

---
+++ discord-bot-project/features/musicPlayback/spotify.js
@@ -0,0 +1,6 @@
+const playFromSpotify = (songName, userCredentials) => {
+    // Logic to play music from Spotify using user's credentials
+    console.log(`Playing ${songName} from Spotify for user: ${userCredentials.username}`);
+};
+
+module.exports = { playFromSpotify };
